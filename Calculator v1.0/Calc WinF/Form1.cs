ea7ab1c0b969ae6cb1bdee8b9c9009375518d13e
FMishka <51850577+FMishka@users.noreply.github.com>
2020-08-25 22:54:06 +0300

    Add files via upload

---
+++ Calculator v1.0/Calc WinF/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc_WinF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstNumber, secondNumber;
+            firstNumber = Convert.ToDouble(textBox1.Text);
+            secondNumber = Convert.ToDouble(textBox2.Text);
+            switch (comboBox1.Text)
+            {
+                case "+":
+                    textBox3.Text = Convert.ToString(firstNumber + secondNumber);
+                    break;
+                case "-":
+                    textBox3.Text = Convert.ToString(firstNumber - secondNumber);
+                    break;
+                case "*":
+                    textBox3.Text = Convert.ToString(firstNumber * secondNumber);
+                    break;
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        MessageBox.Show("На ноль делить нельзя!");
+                    }
+                    else
+                    {
+                        textBox3.Text = Convert.ToString(firstNumber / secondNumber);
+                        break;
+                    }
+                    break;
+                default:
+                    textBox3.Text = "Допущен неверный символ!";
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
